154e5c85d0582ef6b094bfed60efef7fc689e37d
Felipe Alves <profelipealves@outlook.com>
2022-07-04 07:25:00 -0300

    implementando javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+const slider = document.querySelector(".items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return; //parar o fn de ser executado
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-rápido
+  slider.scrollLeft = scrollLeft - walk;
+});
